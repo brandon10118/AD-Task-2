5b9dfd344b2776dd889a2f9740aa0e0e3420a848
brandon10118 <202310118@fit.edu.ph>
2025-05-17 20:19:00 +0800

    feat(landing-page): Added Home button

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
     echo "<header>";
         echo "<div class='navbar'>";
             echo "<h7><a href='/AD-Task-2/index.php'>Zodiac Sign</a></h7>";
+            echo "<a href="">Home</a>
 
         echo "</div>";
     echo "</header>";
