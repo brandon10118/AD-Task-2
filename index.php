f12cfff38fd301f5b67a91cc4427f199a64a95ef
brandon10118 <202310118@fit.edu.ph>
2025-05-17 20:14:50 +0800

    fix(landing-page): Fix wrong code structure

--- index.php
+++ index.php
@@ -8,12 +8,12 @@
 <body>
     <?php
     
-    echo "<header>"
-        echo "<div class='navbar'>"
-            echo "<h7><a href='/AD-Task-2/index.php'>Zodiac Sign</a></h7>"
+    echo "<header>";
+        echo "<div class='navbar'>";
+            echo "<h7><a href='/AD-Task-2/index.php'>Zodiac Sign</a></h7>";
 
-        echo "</div>"
-    echo "</header>"
+        echo "</div>";
+    echo "</header>";
     ?>
 </body>
 </html>
