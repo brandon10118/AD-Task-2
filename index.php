f5e75943d8f2ed333f4c33e1916838a5d6e68288
brandon10118 <202310118@fit.edu.ph>
2025-05-18 15:49:03 +0800

    refactor(landing-page): Change directory

--- index.php
+++ index.php
@@ -4,15 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Zodiac Sign</title>
-    <link rel="stylesheet" href="/AD-Task-2/assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
     <?php
 
     echo "<header>";
         echo "<div class='navbar'>";
-            echo "<h7><a href='/AD-Task-2/index.php'>Zodiac Sign</a></h7>";
-            echo "<a href='/AD-Task-2/index.php' id='Home'>Home</a>";
+            echo "<h7><a href='index.php'>Zodiac Sign</a></h7>";
+            echo "<a href='index.php' id='Home'>Home</a>";
             echo "<a href='/page/zodiacsignlists/index.php' id='ZodiacSignList'>Zodiac Signs List</a>";
         echo "</div>";
     echo "</header>";
