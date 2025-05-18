bb13b853bbd802efd27fbeff7eed26cdd0ddfb0f
brandon10118 <202310118@fit.edu.ph>
2025-05-18 22:29:47 +0800

    feat(zodiaclist): Add Gemini entry

--- handlers/zodiaclist.php
+++ handlers/zodiaclist.php
@@ -3,6 +3,7 @@ $ZodiacSigns = array(
     "Aquarius (January 20 - February 18)",
     "Pisces (February 19 - March 20",
     "Aries (March 21 - April 19)",
-    "Taurus (April 20 - May 20)"
+    "Taurus (April 20 - May 20)",
+    "Gemini (May 21 - June 20"
 );
 ?>
