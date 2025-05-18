3e6f01a5e08d937c5c36acb51286c41a7c79af7f
brandon10118 <202310118@fit.edu.ph>
2025-05-18 15:32:04 +0800

    refactor(landing-page): Change hyperlink location

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     echo "<header>";
         echo "<div class='navbar'>";
             echo "<h7><a href='/AD-Task-2/index.php'>Zodiac Sign</a></h7>";
-            echo "<a href='' id='Home'>Home</a>";
+            echo "<a href='/AD-Task-2/index.php' id='Home'>Home</a>";
             echo "<a href='' id='ZodiacSignList'>Zodiac Signs List</a>";
         echo "</div>";
     echo "</header>";
