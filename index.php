90793828ed576e24a92ddf95d6e18e3fe203a97d
brandon10118 <202310118@fit.edu.ph>
2025-05-18 15:39:46 +0800

    refactor(2nd page): Added directory

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
         echo "<div class='navbar'>";
             echo "<h7><a href='/AD-Task-2/index.php'>Zodiac Sign</a></h7>";
             echo "<a href='/AD-Task-2/index.php' id='Home'>Home</a>";
-            echo "<a href='' id='ZodiacSignList'>Zodiac Signs List</a>";
+            echo "<a href='/page/zodiacsignlists/index.php' id='ZodiacSignList'>Zodiac Signs List</a>";
         echo "</div>";
     echo "</header>";
     ?>
