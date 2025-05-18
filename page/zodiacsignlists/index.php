40140a6c77eb0465dd8dbc99ac8a2d11da2e9a20
brandon10118 <202310118@fit.edu.ph>
2025-05-18 23:23:29 +0800

    refactor(zodiaclist): Replace foreach loop with runlist function call

--- page/zodiacsignlists/index.php
+++ page/zodiacsignlists/index.php
@@ -19,9 +19,7 @@
 
     <?php
     include __DIR__ . '/../../handlers/zodiaclist.handler.php';
-    foreach ($ZodiacSigns as $ZodiacSign){
-        echo $ZodiacSign;
-    }
+    runlist();
     ?>
 
 </body>
