ec0af3a939a5e7abbbae16ea9230c2be9fa5a2c9
brandon10118 <202310118@fit.edu.ph>
2025-05-18 21:59:18 +0800

    feat(zodiac): add stylesheet link for zodiac list handler

--- page/zodiacsignlists/index.php
+++ page/zodiacsignlists/index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Zodiac Sign List</title>
     <link rel="stylesheet" href="/page/zodiacsignlists/assets/css/style.css">
+    <link rel="stylesheet" href="/handlers/zodiaclist.php">
 </head>
 <body>
     <?php
