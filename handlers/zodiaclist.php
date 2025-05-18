1ee3dc930de8e6c04a6d961d027384e3ff30a839
brandon10118 <202310118@fit.edu.ph>
2025-05-18 22:28:29 +0800

    feat(zodiaclist): Add Pisces entry to ZodiacSigns array

--- handlers/zodiaclist.php
+++ handlers/zodiaclist.php
@@ -1,6 +1,7 @@
 <?php
 $ZodiacSigns = array(
     "Aquarius (January 20 - February 18)",
+    "Pisces (February 19 - March 20",
     "Aries (March 21 - April 19)",
     "Taurus (April 20 - May 20)"
 );
