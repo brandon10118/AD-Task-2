c5c443fdd0a56f47e19b1db49de7d8d94a44368d
brandon10118 <202310118@fit.edu.ph>
2025-05-18 22:31:54 +0800

    feat(zodiaclist): Add Leo entry to ZodiacSigns array

--- handlers/zodiaclist.php
+++ handlers/zodiaclist.php
@@ -5,6 +5,7 @@ $ZodiacSigns = array(
     "Aries (March 21 - April 19)",
     "Taurus (April 20 - May 20)",
     "Gemini (May 21 - June 20",
-    "Cancer (June 21 - July 22)"
+    "Cancer (June 21 - July 22)",
+    "Leo (July 23 - August 22"
 );
 ?>
